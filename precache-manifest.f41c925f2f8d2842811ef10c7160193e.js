da53a197b0ea545f4ff6d531b4773d88427a9db6
personal computer <bret.allan@gmail.com>
2019-04-29 09:42:38 -0700

    Updates

---
+++ precache-manifest.f41c925f2f8d2842811ef10c7160193e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85a8546fecf5d6de1fd1620f4031af7",
+    "url": "/react_click_app/index.html"
+  },
+  {
+    "revision": "c2bec578ab56cde123f5",
+    "url": "/react_click_app/static/css/main.739eb5b1.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/react_click_app/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "c2bec578ab56cde123f5",
+    "url": "/react_click_app/static/js/main.ee5a1465.chunk.js"
+  },
+  {
+    "revision": "1cc89781c5edb5ac45e3",
+    "url": "/react_click_app/static/js/runtime~main.ea0c875b.js"
+  }
+]);
